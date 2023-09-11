ae1a575b1f57e16499afd6189d77eb39db081b31
Abhishek Kumar <anu250867anu@gmail.com>
2023-09-12 01:54:46 +0530

    create landing page

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+// Footer component for footer section
+const Footer = () => {
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a href="https://www.linkedin.com/in/abckyadav/" target="_blank">
+        Abhishek Kumar
+      </a>
+      <i className="fa-solid fa-copyright"></i>2023
+      <strong>
+        Food<span>Villa</span>
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
